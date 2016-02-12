8ad5fd7719b9e38b4ebf90696fc118fcb4de99ac
michael <m.makurov@gmail.com>
2016-02-12 21:45:45 +0300

    Dybenko 12.02.2016 21:45

--- 17.Generics.L18.HeapPollution/src/HeapPollutionExample.java
+++ 17.Generics.L18.HeapPollution/src/HeapPollutionExample.java
@@ -1,12 +1,15 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by user on 12.02.2016.
  */
 public class HeapPollutionExample {
     public static void main(String[] args) {
+//        heapPollutionExampleOne();
+        heapPollutionExampleTwo();
+    }
+/*
+    public static void heapPollutionExampleOne() {
         List<String> stringListA = new ArrayList<String>();
         List<String> stringListB = new ArrayList<String>();
         ArrayBuilder.addToList(stringListA, "Seven", "Eight", "Nine");
@@ -15,6 +18,17 @@ public class HeapPollutionExample {
         ArrayBuilder.addToList(listOfStringLists, stringListA, stringListB);
 
         ArrayBuilder.faultyMethod(Arrays.asList("Hello!"), Arrays.asList("World!"));
+    }
+*/
+    public static void heapPollutionExampleTwo() {
+        Set rawS = new TreeSet<Integer>();
+        Set<String> parameterizedS = rawS;
+        rawS.add(new Integer(42));
+        Iterator<String> iterator = parameterizedS.iterator();
+        while(iterator.hasNext()) {
+            String string = iterator.next();
+            System.out.println(string);
+        }
     }
 }
 
